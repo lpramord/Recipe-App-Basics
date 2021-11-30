3b46d0a075b572c6d5d52d06668e4fe2b802a429
lpramord <lpramord@gmail.com>
2021-11-30 17:07:39 +0530

    fecth all data from api and let it to a variable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const AppId = "04c465cc";
+  const AppKey = "1c83f9971e2e618fcb3f6e45d56e737c";
+
+  const [resipe,setRecipe] = useState([]);
+
+  useEffect( ()=>{
+    getRecipies();
+  },[]);
+
+  const getRecipies = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${AppId}&app_key=${AppKey}`);
+    const data = await response.json();
+    setRecipe(data.hits);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="serch">
+        <input className="serchImput"></input>
+        <button className="serchButton" type="submit">Serch</button>
+      </form>
     </div>
-  );
+  )
+
 }
 
 export default App;
