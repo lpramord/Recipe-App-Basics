700b640e191ad423e68337b613328b168c93aa65
lpramord <lpramord@gmail.com>
2021-12-01 10:38:57 +0530

    styling frist step

--- src/repcipeTemplate.js
+++ src/repcipeTemplate.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import style from "./recipe.module.css"
 
 const repcipeTemplate = ({tittle , calaries , image , ingredients})=>{
     return(
         <div className="recipeTem">
-            <h1 className="name">{tittle}</h1>
+            <h1 className={style.name}>{tittle}</h1>
             <img src={image} alt="" className="img"></img>
             <p className="calories">{calaries}</p>
             {ingredients.map((ingredient,i)=>(
